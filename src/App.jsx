0247991421caf57c0d92c4af6aa13aed0ce73a04
Sharmin Sultana <sharmin9518@gmail.com>
2024-12-15 13:59:44 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import Features from "./components/Features"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import HowItWorks from "./components/HowItWorks"
+import Model from "./components/Model"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      
+    </main>
+    
   )
 }
 
